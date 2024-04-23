ba972f8e71b840beb541a106b4339576cc943e4f
Samuele Leoni <leo22009@byui.edu>
2024-04-23 13:31:19 +0200

    Finished W01 Individual activity

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -3,14 +3,22 @@ public static class ArraySelector
     public static void Run()
     {
         var l1 = new[] { 1, 2, 3, 4, 5 };
-        var l2 = new[] { 2, 4, 6, 8, 10};
-        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1};
+        var l2 = new[] { 2, 4, 6, 8, 10 };
+        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1 };
         var intResult = ListSelector(l1, l2, select);
         Console.WriteLine("<int[]>{" + string.Join(", ", intResult) + "}"); // <int[]>{1, 2, 3, 2, 4, 4, 6, 8, 10, 5}
     }
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return new int[0];
+        int[] arr = new int[list1.Length + list2.Length];
+
+
+        for (int i = 0, idx1 = 0, idx2 = 0; i < select.Length; i++)
+        {
+            arr[i] = select[i] == 1 ? list1[idx1++] : list2[idx2++];
+        }
+
+        return arr;
     }
 }
