01b3174a90f92636a5cf3f2ab570ad6743d7333a
Samuele Leoni <leo22009@byui.edu>
2024-04-28 19:34:22 +0200

    W02 Learning Activities Done

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -10,7 +10,10 @@
         queue.Enqueue(100);
         var value = queue.Dequeue();
         Console.WriteLine(value);
-        // Defect(s) Found:
+        // Defect(s) Found: Test 1 would crash when calling Dequeue because the function had two errors,
+        // on both the dereferencing of the element and the RemoveAt function call, the index used was 1
+        // although the first element in Arrays is index = 0. The function would throw an 
+        // ArgumentOutOfRangeException
 
         Console.WriteLine("------------");
 
@@ -28,7 +31,8 @@
         Console.WriteLine(value);
         value = queue.Dequeue();
         Console.WriteLine(value);
-        // Defect(s) Found: 
+        // Defect(s) Found: The Enqueue function was wrong, it was inserting the new element at index 0
+        // instead of appending it to the end as requested
 
         Console.WriteLine("------------");
 
@@ -44,7 +48,7 @@
         catch (IndexOutOfRangeException) {
             Console.WriteLine("I got the exception as expected.");
         }
-        // Defect(s) Found: 
+        // Defect(s) Found: None
     }
 
     private readonly List<int> _queue = new();
@@ -54,7 +58,7 @@
     /// </summary>
     /// <param name="value">Integer value to add to the queue</param>
     private void Enqueue(int value) {
-        _queue.Insert(0, value);
+        _queue.Add(value);
     }
 
     /// <summary>
@@ -66,8 +70,8 @@
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        var value = _queue[0];
+        _queue.RemoveAt(0);
         return value;
     }
 }
