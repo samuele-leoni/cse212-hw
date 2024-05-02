e18a432c76a27c274e77334f5f6a5285d3006843
Samuele Leoni <leo22009@byui.edu>
2024-05-02 20:19:07 +0200

    Finished team activity

--- week02/teach/CustomerService.cs
+++ week02/teach/CustomerService.cs
@@ -11,20 +11,58 @@ public class CustomerService {
         // Test Cases
 
         // Test 1
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: Two Customer Service Queue will be created with different maxSize parameter passed. 
+        // The first will have maxSize = 0 and the second maxSize = -1.
+        // Expected Result: The first should have maxSize = 10.
         Console.WriteLine("Test 1");
 
-        // Defect(s) Found: 
+        var customerServiceQueue = new CustomerService(0);
+        Console.WriteLine(customerServiceQueue._maxSize == 10 ? "The queue with maxSize = 0 works!" : "ERROR: The queue with maxSize = 0 doesn't work!");
+
+        customerServiceQueue = new CustomerService(-1);
+        Console.WriteLine(customerServiceQueue._maxSize == 10 ? "The queue with maxSize = -1 works!" : "ERROR: The queue with maxSize = -1 doesn't work!");
+
+        // Defect(s) Found: None
 
         Console.WriteLine("=================");
 
         // Test 2
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: A maxSize variable of 3 will be created and then a for loop will cycle one time more than the
+        // maxSize value adding a new customer using the method AddNewCustomer.
+        // Expected Result: The queue should have 3 customers and then throw an error for the sixth.
         Console.WriteLine("Test 2");
 
-        // Defect(s) Found: 
+        var maxSize = 3;
+        customerServiceQueue = new CustomerService(maxSize);
+
+        for (int i = 0; i <= maxSize; i++)
+        {
+            customerServiceQueue.AddNewCustomer();
+        }
+
+        Console.WriteLine(customerServiceQueue._queue.Count == maxSize ? "The queue has 3 customers!" : $"ERROR: The queue has {customerServiceQueue._queue.Count} customers instead of 3!");
+
+        // Defect(s) Found: The condition was checking if _queue.Count was greather than maxSize but it should've checked
+        // when it becomes equal.
+
+        Console.WriteLine("=================");
+
+        // Test 3
+        // Scenario: Using the previously added customers the ServeCustomer method will be called one time more than
+        // the queue actual content.
+        // Expected Result: The queue should have 0 customers and throw an error for the last call of ServeCustomer.
+        Console.WriteLine("Test 3");
+
+        for (int i = 0; i <= maxSize; i++)
+        {
+            customerServiceQueue.ServeCustomer();
+        }
+
+        Console.WriteLine(customerServiceQueue._queue.Count == 0 ? "The queue is empty, it works!" : $"ERROR: The queue has {customerServiceQueue._queue.Count} customers instead of 0!");
+
+        // Defect(s) Found: The whole content of the ServeCustomer method should be wrapped in a try catch to handle
+        // the ArgumentOutOfRangeException and the customer was stored in a variable after removing it which would cause
+        // the console to print out the wrong customer.
 
         Console.WriteLine("=================");
 
@@ -67,7 +105,7 @@ public class CustomerService {
     /// </summary>
     private void AddNewCustomer() {
         // Verify there is room in the service queue
-        if (_queue.Count > _maxSize) {
+        if (_queue.Count >= _maxSize) {
             Console.WriteLine("Maximum Number of Customers in Queue.");
             return;
         }
@@ -88,9 +126,16 @@ public class CustomerService {
     /// Dequeue the next customer and display the information.
     /// </summary>
     private void ServeCustomer() {
-        _queue.RemoveAt(0);
-        var customer = _queue[0];
-        Console.WriteLine(customer);
+        try
+        {
+            var customer = _queue[0];
+            _queue.RemoveAt(0);
+            Console.WriteLine(customer);
+        }
+        catch(ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Queue already empty!");
+        }
     }
 
     /// <summary>
