16ac51a2bc76250dec25df3fe4bca9035a3fa2e1
Samuele Leoni <leo22009@byui.edu>
2024-05-09 21:02:07 +0200

    W03 Finished team activity

--- week03/teach/DisplaySums.cs
+++ week03/teach/DisplaySums.cs
@@ -1,5 +1,7 @@
-﻿public static class DisplaySums {
-    public static void Run() {
+﻿public static class DisplaySums
+{
+    public static void Run()
+    {
         DisplaySumPairs(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
         // Should show something like (order does not matter):
         // 6 4
@@ -27,7 +29,26 @@
     /// in the list.
     /// </summary>
     /// <param name="numbers">array of integers</param>
-    private static void DisplaySumPairs(int[] numbers) {
+    private static void DisplaySumPairs(int[] numbers)
+    {
         // TODO Problem 2 - This should print pairs of numbers in the given array
+
+        var myFullSet = numbers.ToHashSet();
+        var mySet = new HashSet<int>();
+        foreach (int number in numbers)
+        {
+            var secondNumber = 10 - number;
+            if (number != 5 && myFullSet.Contains(secondNumber))
+            {
+                if (!mySet.Contains(number))
+                {
+                    mySet.Add(number);
+                    mySet.Add(secondNumber);
+                    Console.WriteLine($"{number} {secondNumber}");
+                }
+
+            }
+
+        }
     }
 }
